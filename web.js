e241786a8a3a6cb6f5b659be035af83a822e002e
Robin Joseph <robinjoseph08@sbcglobal.net>
2013-09-15 14:45:43 -0500

    stuff

--- web.js
+++ web.js
@@ -3,7 +3,6 @@
 /***************************************/
 
 var net = require('net')
-  , mongo = require('mongojs')
   , db = {
     cups: [{
       id: 1,
@@ -11,13 +10,21 @@ var net = require('net')
       empty: false,
       drink: 'Dr. Pepper',
       assigned: null
+    },{
+      id: 2,
+      table_id: 3,
+      empty: false,
+      drink: 'Pepsi',
+      assigned: null
     }]
   }
-  , express = require('express')
-  , app = express()
-  , http = require('http')
-  , http_server = http.createServer(app)
-  , io = require('socket.io').listen(http_server)
+  // , app = express()
+  // , http = require('http')
+  // , http_server = http.createServer(app)
+  , UA = require("urban-airship")
+  , ua = new UA("LXiU5zf5T828zvehhEYztw", "LyuvmcX7T-KJWWH7ubtTkA", "1SqY61axQPGpA2-awcJpsw")
+  , client_socket
+  , token
   ;
 
 /***************************************/
@@ -25,28 +32,53 @@ var net = require('net')
 /***************************************/
 
 net_server = net.createServer(function(socket) {
+  console.log('in net_server');
+  socket.write('butt\r\n');
   socket.on('data',function(data) {
     var data_str = '' + data;
-    if(data_str.indexOf('{') != -1) {
+    console.log('in net_server data');
+    console.log(data_str);
+    if(data_str.indexOf('token=') != -1) {
+      client_socket = socket;
+      token = data_str.substring(data_str.indexOf('='),data_str.length);
+      ua.registerDevice(token,function(err){});
+    } else if(data_str.indexOf('{') != -1) {
       var cup_json = JSON.parse(data_str.substring(data_str.indexOf('{'),data_str.length));
       console.log(cup_json);
-      if(!db.cups[cup_json.cup_id].empty) {
-        db.cups[cup_json.cup_id].empty = true;
+      if(cup_json.cup_id && !db.cups[cup_json.cup_id-1].empty) {
+        db.cups[cup_json.cup_id-1].empty = true;
+        console.log('Cup ' + cup_json.cup_id + ' is empty.');
         // send notification to iOS
-        // socket.emit('refill', {table_id: db.cups[cup_json.cup_id].table_id});
+        // socket.emit('refill', {table_id: db.cups[cup_json.cup_id-1].table_id});
+        if(client_socket) {
+          client_socket.write('refill,' + db.cups[cup_json.cup_id-1].table_id + ',' + db.cups[cup_json.cup_id-1].drink + '\r\n');
+          var payload1 = {
+            "aps": {
+              "badge": 1,
+              "alert": "Refill Table " + db.cups[cup_json.cup_id-1].table_id + ' with ' + db.cups[cup_json.cup_id-1].drink,
+              "sound": "ding"
+            }
+          };
+          ua.pushNotification("/api/push/broadcast/", payload1, function(error) {console.log(error)});
+        }
+      } else if(cup_json.filled && db.cups[cup_json.filled-1].empty) {
+        db.cups[cup_json.filled-1].empty = false;
+        db.cups[cup_json.filled-1].assigned = null;
+        console.log('Cup ' + cup_json.filled + ' is filled.');
+        // send notification to iOS
+        if(client_socket) {
+          client_socket.write('filled,' + db.cups[cup_json.filled-1].table_id + '\r\n');
+        }
       }
     }
   });
+  socket.on('close',function() {
+    console.log('socket closed');
+    client_socket = null;
+  });
 });
 
-/***************************************/
-/***             ROUTES              ***/
-/***************************************/
-
-app.get('/',function(req,res) {
-  console.log('GET /');
-  res.send('YAY HTTP!');
-});
+var io = require('socket.io').listen(net_server)
 
 /***************************************/
 /***             LISTEN              ***/
@@ -56,28 +88,17 @@ net_server.listen(5000,function() {
   console.log('TCP now listening on port 5000.');
 });
 
-http_server.listen(3000,function() {
-  console.log('HTTP now listening on port 3000.');
-});
-
-
 /***************************************/
 /***            SOCKET.IO            ***/
 /***************************************/
 
-io.sockets.on('connection', function(socket) {
+/*io.sockets.on('connection', function(socket) {
+  console.log('socket connected');
   socket.on('assign',function(data) {
+    console.log('socket assign');
     var assign_json = JSON.parse('' + data);
     if(db.cups[assign_json.cup_id-1].empty) {
       db.cups[assign_json.cup_id-1].assigned = assign_json.name;
     }
   });
-
-  socket.on('clear',function(data) {
-    var clear_id = Number(data);
-    if(db.cups[clear_id-1].empty) {
-      db.cups[clear_id-1].empty = false;
-      db.cups[clear_id-1].assigned = null;
-    }
-  });
-});
+});*/
